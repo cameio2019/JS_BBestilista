06d1f914fde086e71bc34505463b557561a3e611
zeckedev <hlfoeste@gmail.com>
2021-07-02 02:52:27 -0300

    iniciando project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function solicitar(){
+    let noMbre = prompt ("Ingresa tu nombre");
+    let apeLlido = prompt ("Ingresa tu Apellido");
+    let dIa = prompt ("Ingrese día");
+    let mes = "junio";
+    
+    alert(noMbre + " " + "Tu turno es el día" + dIa + " " + mes); 
+}
+
+
---
+++ js/sweetA.js
@@ -0,0 +1,12 @@
+Swal.fire({
+    title: 'Bienvenido!',
+    text: 'Por favor realizá tu reserva de Turno.',
+    icon: 'info',
+    confirmButtonText: 'Aceptar',
+    showCloseButton: true,
+    backdrop: true,
+    timer: 5000,
+	timerProgressBar: true,
+    allowOutsideClick: false,
+}
+)
