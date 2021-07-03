86d871246a4d7daf33f07ce9bd8f6a46380814c8
zeckedev <hlfoeste@gmail.com>
2021-07-03 00:49:31 -0300

    update

--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@ function solicitar(){
     let noMbre = prompt ("Ingresa tu nombre");
     let apeLlido = prompt ("Ingresa tu Apellido");
     let dIa = prompt ("Ingrese día");
-    let hoRa = prompt("Ingresa la hora")
+    let hoRa = prompt("Ingresa la hora");
     let mes = "junio";
-    
     
     alert(noMbre + " Tu turno es el día" + dIa + " de " + mes + " a las " + hoRa + " hs.");
     Swal.fire(noMbre + " Gracias por tu Reserva!" + " Te esperamos" + " 😃.");
