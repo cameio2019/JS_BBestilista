78e52fedde55290116a90cd3dba171affbdd0f5a
ZeckeDev <hlfoeste@gmail.com>
2021-07-28 23:23:36 -0300

    desaf 6

--- js/main.js
+++ js/main.js
@@ -111,4 +111,79 @@ function tiposCortes(){
         }
 } 
 
+//Desafio 6
+//Buscar y encontrar servicios
+const servicios =   [{id: 1, servicio: "Corte", precio: 1000},
+                    {id: 2, servicio: "Color", precio: 500},
+                    {id: 3, servicio: "Peinado", precio: 1500},
+                    {id: 4, servicio: "Alisado", precio: 700},
 
+];
+
+const buscarCorte = servicios.find(servicio => servicio.id === 1);
+console.log(buscarCorte);
+
+const buscarColor = servicios.find(servicio => servicio.id === 2);
+console.log(buscarColor);
+
+const buscarPeinado = servicios.find(servicio => servicio.id === 3);
+console.log(buscarPeinado);
+
+const buscarAlisado = servicios.find(servicio => servicio.id === 4);
+console.log(buscarAlisado);
+
+//Filtar Peluquero
+const peluqueros = [{id: 1, peluquero: "Bruno"},
+                    {id: 2, peluquero: "Pepe"},
+                    {id: 3, peluquero: "Juan"},
+];
+
+const filtrarPeluquero1 = peluqueros.filter(peluquero => peluquero.id === 1);
+console.log(filtrarPeluquero1);
+
+const filtrarPeluquero2 = peluqueros.filter(peluquero => peluquero.id === 2);
+console.log(filtrarPeluquero2);
+
+const filtrarPeluquero3 = peluqueros.filter(peluquero => peluquero.id === 3);
+console.log(filtrarPeluquero3);
+//Desafio 6
+
+//Desafio Complementario - Ordenar array de obj
+peluqueros.sort((i1, i2) => {
+    if (i1.peluquero < i2.peluquero) {
+        return 1;
+        } else if (i1.peluquero > i2.peluquero) {
+            return -1;
+        } else {
+            return 0;
+        }
+}); 
+
+console.log(peluqueros);
+
+//Buscar Producto en Shop
+const productos =   [{id: 1, producto: "A", precio: 1000},
+                    {id: 2, producto: "B", precio: 500},
+                    {id: 3, producto: "C", precio: 1500},
+                    {id: 4, producto: "D", precio: 700},
+];
+
+const ingresoProductoA = productos.find(producto => producto.id === 1);
+console.log(ingresoProductoA);
+
+const ingresoProductoB = productos.find(producto => producto.id === 2);
+console.log(ingresoProductoB);
+
+const ingresoProductoC = productos.find(producto => producto.id === 3);
+console.log(ingresoProductoC);
+
+//Ordenar por precios de Prod en Shop Section
+let ordenPrecios = [];
+ordenPrecios = productos.map(e => e);
+let ordPrecios = productos;
+ordenPrecios.sort((p1, p2) => {
+    return p1.precio - p2.precio;
+});
+
+console.log("Precios ordenados en forma Ascendente");
+console.log(ordenPrecios);
